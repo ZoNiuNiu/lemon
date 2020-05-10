d6eabd1c00d655b24bfa83e0267e946882490d60
ZoNiuNiu <izhongxiangang@163.com>
2020-05-10 14:47:18 +0800

    Signed-off-by: ZoNiuNiu <izhongxiangang@163.com> 20200510-1447

---
+++ python_study/string.py
@@ -0,0 +1,27 @@
+import turtle
+import time
+# t = turtle.Pen()
+# for x in range(1, 120, 2):
+#     t.forward(x)
+#     t.left(59)
+
+a = time.time()
+
+if a.__eq__(time.time()):
+    print(True)
+
+
+a = ['nih', 'zsd', 'xcv']
+
+b = ''.join(a)
+
+print(b)
+
+"""
+len()
+startswith()
+endswith()
+str.find()
+str.rfind()
+str.strip()
+"""
