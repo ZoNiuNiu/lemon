f4dc2416ede051870576a23ae53c22767ba2be84
ZoNiuNiu <izhongxiangang@163.com>
2020-05-11 15:14:53 +0800

    Signed-off-by: ZoNiuNiu <izhongxiangang@163.com> 20200511-1514

--- python_study/class_0511.py
+++ python_study/class_0511.py
@@ -4,3 +4,13 @@ my_text = 'i love you, i love sxt, i love you'
 
 a = {c:my_text.count(c) for c in my_text if my_text.count(c)>1}
 print(a)
+
+
+def print_max(xa,xb):
+    if xa>xb:
+        print(xa,'较大值')
+    else:
+        print(xb,'较大值')
+    print(id(xa))
+
+print_max(10,20)
